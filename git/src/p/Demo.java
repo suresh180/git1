7e403e70fba9a59bf9e2bca3dacefd3fc1fa7379
sures <sures@LAPTOP-954R2NVB>
2021-05-04 10:42:32 +0530

    added one line

--- git/src/p/Demo.java
+++ git/src/p/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
       System.out.println("Hello git welcome to the world");
+      System.out.println("inside main method");
 	}
 
 }
